c03b7239f760445125ef8a7f0037da41d2f401fd
Yozhylo <polidar2012@gmail.com>
2021-04-28 17:58:20 +0300

    Updated

--- HW1/battleships.js
+++ HW1/battleships.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -8,21 +9,26 @@ var isSunk = false;
 
 while (isSunk == false)
 {
-    guess = promt("Ready, aim, fire! (enter a number from 0-6):);
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
     
-    if (guess < 0 || guess > 6) alert("Please enter a valid cell number!");
-    else
-	{
-        alert("HIT!");
-        hits += 1;
-    
-	    if (hits == 3)
+    if (guess < 0 || guess > 6) alert("Please enter a valid cell number!")
+    else 
+    {
+        guesses += 1;
+
+        if (guess == location1 || guess == location2 || guess == location3)
         {
-            isSunk = true;
-			alert("You sank my battleship!");
+            alert("HIT!");
+            hits += 1;
+
+            if (hits == 3)
+            {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
         }
-	}
-	else alert("MISS!");
+        else alert("MISS!");
+    }
 }
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your accuracy was " + (3/guesses);
 alert(stats);
