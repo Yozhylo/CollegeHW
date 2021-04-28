2762275b9d9ae668424cc745900005d789026bb7
Yozhylo <polidar2012@gmail.com>
2021-04-28 20:31:04 +0300

    Final

--- HW2/hw2.js
+++ HW2/hw2.js
@@ -5,7 +5,7 @@ while(Q)
     let choice = prompt(menu);
     switch(choice)
     {
-        case '1':
+        case '1': //Задача 1
         {
             alert("Executing Task 1...");
 
@@ -20,7 +20,7 @@ while(Q)
         }
         break;
 
-        case '2':
+        case '2': //Задача 2
         {
             alert("Executing Task 2...")
 
@@ -30,51 +30,51 @@ while(Q)
         }
         break;
 
-        case '3':
+        case '3': //Задача 3
         {
             let login = prompt("Please enter your login.");
-        switch(login)
-            {
-                case "ivan":
-                    {
-                    let passwd = prompt("Please enter your password.");
-                    if (passwd == '333') alert("Welcome back!")
-                    else alert("Invalid password!")
-                    }
-                break;
+            switch(login) //Кривое ветвление, тут нужна функция
+                {
+                    case "ivan":
+                        {
+                            let passwd = prompt("Please enter your password.");
+                            if (passwd == '333') alert("Welcome back!")
+                            else alert("Invalid password!")
+                        }
+                    break;
 
-                case "ssss":
-                    {
-                    let passwd = prompt("Please enter your password.");
-                    if (passwd == '666') alert("Welcome back!")
-                    else alert("Invalid password!")
-                    }
-                break;
+                    case "ssss":
+                        {
+                            let passwd = prompt("Please enter your password.");
+                            if (passwd == '666') alert("Welcome back!")
+                            else alert("Invalid password!")
+                        }
+                    break;
 
-                case "gibs":
-                    {
-                    let passwd = prompt("Please enter your password.");
-                    if (passwd == '0000') alert("Welcome back!")
-                    else alert("Invalid password!")
-                    }
-                break; 
+                    case "gibs":
+                        {
+                            let passwd = prompt("Please enter your password.");
+                            if (passwd == '0000') alert("Welcome back!")
+                            else alert("Invalid password!")
+                        }
+                    break; 
 
-                default: alert("User not found!");
-                break;
-            }
+                    default: alert("User not found!");
+                    break;
+                }
         }
         break;
 
-        case '4':
+        case '4': //Задача 4
         {
-        let num1 = Number(prompt("Enter the first number to be compared"));
-        let num2 = Number(prompt("Enter the second number to be compared"));
-        let num3 = Number(prompt("Enter the third number to be compared"));
+            //Объявление переменных, тут бы массив и цикл на ввод
+            let num1 = Number(prompt("Enter the first number to be compared"));
+            let num2 = Number(prompt("Enter the second number to be compared"));
+            let num3 = Number(prompt("Enter the third number to be compared"));
+            let max = Math.max(num1, num2, num3);
 
-        if (num1 > num2 && num1 > num3) alert(num1 + " is the greatest number!")
-        else if (num2 > num1 && num2 > num3) alert(num2 + " is the greatest number!")
-        else if (num3 > num1 && num3 > num2) alert(num3 + " is the greatest number!")
-        else alert("All numbers are equal!");
+            if (max != num1 && max != num2 && max != num3) alert(max + " is the greatest number!")
+            else alert("All numbers are equal!");
         }
         break;
 
