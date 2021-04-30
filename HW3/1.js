6731a01fb52a232924397f168b1d2bc9f21a28c5
Yozhylo <polidar2012@gmail.com>
2021-04-30 19:20:17 +0300

    Final

--- HW3/1.js
+++ HW3/1.js
@@ -1,6 +1,5 @@
 function plus()
 {
-<<<<<<< HEAD
     var num1, num2, result;
     num1 = document.getElementById('n1').value;
     num1 = parseInt(num1);
@@ -23,8 +22,4 @@ function minus();
     result = num1 - num2;
     document.getElementById('out').innerHTML = result;
 }
-=======
-var num1, num2, result;
-num1 = document.getElementById('n1').value;
-}
->>>>>>> 4462728e7f3e9e36f459e0a0401c75469727810a
+
